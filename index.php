5161f61078f41da789c5607ee95bebbfc10356a6
ilhamsofdya <42112832+ilhamsofdya@users.noreply.github.com>
2022-05-30 22:57:30 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$path = "https://api.telegram.org/bot5355398612:AAHtMgc6a-0gGR4U4uByVYU13a3rQqbe03k;
+
+$update = json_decode(file_get_contents("php://input"), TRUE);
+
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+
+if (strpos($message, "/weather") === 0) {
+$location = substr($message, 9);
+$weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
+file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+}
+?>
