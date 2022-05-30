e7cacbcd5e77b2a5a74a7939ed3ac7da56899b64
ilhamsofdya <42112832+ilhamsofdya@users.noreply.github.com>
2022-05-30 23:04:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $message = $update["message"]["text"];
 
 if (strpos($message, "/weather") === 0) {
 $location = substr($message, 9);
-$weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
+$weather = json_decode(file_get_contents("https://affiliate.shopee.co.id/api/v1/affiliates/".$location."), TRUE)["weather"][0]["main"];
 file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
 }
 ?>
